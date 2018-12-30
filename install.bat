9a4d89bf59acfe51d63242b6b54ca05d899a6e40
José Pedro <jpedrolmail@gmail.com>
2018-12-30 22:03:20 +0000

    Build updates.

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 dotnet tool uninstall --global dotnet-properties
 
 dotnet build dotnet-properties.sln -c Release
-dotnet tool install --global --add-source artifacts\Release\nupkg\ --version 0.1.0-* dotnet-properties
+dotnet tool install --global --add-source artifacts\Release\nupkg\ --version 0.2.0-* dotnet-properties
--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 dotnet tool uninstall --global dotnet-properties
 
 dotnet build dotnet-properties.sln -c Release
-dotnet tool install --global --add-source artifacts/Release/nupkg/ --version 0.1.0-* dotnet-properties
+dotnet tool install --global --add-source artifacts/Release/nupkg/ --version 0.2.0-* dotnet-properties
