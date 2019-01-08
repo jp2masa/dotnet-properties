ae054d29873afe55c200ec56a27601f1ad48096a
José Pedro <jpedrolmail@gmail.com>
2019-01-08 23:32:48 +0000

    Updated version to 0.3.0.

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
 dotnet tool uninstall --global dotnet-properties
 
 dotnet build dotnet-properties.sln -c Release
-dotnet tool install --global --add-source artifacts\Release\nupkg\ --version 0.2.0-* dotnet-properties
+dotnet tool install --global --add-source artifacts\Release\nupkg\ --version 0.3.0-* dotnet-properties
