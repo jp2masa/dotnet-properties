d6b6fc621c474cbce3656d358ddcab8a59e223d3
José Pedro <jpedrolmail@gmail.com>
2020-08-15 22:45:48 +0100

    SideBar theme fixes.

--- src/dotnet-properties/Services/ThemeService.cs
+++ src/dotnet-properties/Services/ThemeService.cs
@@ -9,40 +9,52 @@ namespace DotNet.Properties.Services
 {
     internal sealed class ThemeService : IThemeService
     {
-        private static readonly StyleInclude Styles = Style("avares://dotnet-properties/Styles/Styles.xaml");
+        private static readonly Uri StylesBase = new Uri("avares://dotnet-properties/Styles/");
+        private static readonly Uri Accents = new Uri(StylesBase, "Accents/");
+
+        private static readonly Uri AvaloniaDefaultAccents = new Uri("avares://Avalonia.Themes.Default/Accents/");
+        private static readonly Uri AvaloniaFluentAccents = new Uri("avares://Avalonia.Themes.Fluent/Accents/");
+
+        private static readonly Uri CitrusBase = new Uri("avares://Citrus.Avalonia/");
+
+        private static readonly StyleInclude Styles = Style(StylesBase, "Styles.xaml");
 
         private static readonly StyleInclude AvaloniaDefault = Style("avares://Avalonia.Themes.Default/DefaultTheme.xaml");
 
-        private static readonly StyleInclude AvaloniaDefaultBaseLight = AvaloniaDefaultAccent("BaseLight");
-        private static readonly StyleInclude AvaloniaDefaultBaseDark = AvaloniaDefaultAccent("BaseDark");
+        private static readonly StyleInclude AvaloniaDefaultBaseLight = Style(AvaloniaDefaultAccents, "BaseLight.xaml");
+        private static readonly StyleInclude AvaloniaDefaultBaseDark = Style(AvaloniaDefaultAccents, "BaseDark.xaml");
 
-        private static readonly StyleInclude AvaloniaFluentBase = AvaloniaFluentAccent("Base");
-        private static readonly StyleInclude AvaloniaFluentBaseLight = AvaloniaFluentAccent("BaseLight");
-        private static readonly StyleInclude AvaloniaFluentBaseDark = AvaloniaFluentAccent("BaseDark");
+        private static readonly StyleInclude AvaloniaFluentBase = Style(AvaloniaFluentAccents, "Base.xaml");
+        private static readonly StyleInclude AvaloniaFluentBaseLight = Style(AvaloniaFluentAccents, "BaseLight.xaml");
+        private static readonly StyleInclude AvaloniaFluentBaseDark = Style(AvaloniaFluentAccents, "BaseDark.xaml");
 
-        private static readonly StyleInclude AvaloniaFluentLight = AvaloniaFluentAccent("FluentLight");
-        private static readonly StyleInclude AvaloniaFluentDark = AvaloniaFluentAccent("FluentDark");
+        private static readonly StyleInclude AvaloniaFluentLight = Style(AvaloniaFluentAccents, "FluentLight.xaml");
+        private static readonly StyleInclude AvaloniaFluentDark = Style(AvaloniaFluentAccents, "FluentDark.xaml");
 
-        private static readonly StyleInclude BaseLightBlue = Style("avares://dotnet-properties/Styles/Accents/BaseLightBlue.xaml");
+        private static readonly StyleInclude BaseLightBlue = Style(Accents, "BaseLightBlue.xaml");
 
-        private static readonly StyleInclude Citrus = CitrusTheme("Citrus");
-        private static readonly StyleInclude CitrusSea = CitrusTheme("Sea");
-        private static readonly StyleInclude CitrusRust = CitrusTheme("Rust");
-        private static readonly StyleInclude CitrusCandy = CitrusTheme("Candy");
-        private static readonly StyleInclude CitrusMagma = CitrusTheme("Magma");
+        private static readonly StyleInclude Citrus = Style(CitrusBase, "Citrus.xaml");
+        private static readonly StyleInclude CitrusSea = Style(CitrusBase, "Sea.xaml");
+        private static readonly StyleInclude CitrusRust = Style(CitrusBase, "Rust.xaml");
+        private static readonly StyleInclude CitrusCandy = Style(CitrusBase, "Candy.xaml");
+        private static readonly StyleInclude CitrusMagma = Style(CitrusBase, "Magma.xaml");
+
+        private static readonly StyleInclude DefaultThemeAccents = Style(Accents, "DefaultThemeAccents.xaml");
+        private static readonly StyleInclude FluentThemeAccents = Style(Accents, "FluentThemeAccents.xaml");
+        private static readonly StyleInclude CitrusThemeAccents = Style(Accents, "CitrusThemeAccents.xaml");
 
         private static readonly Theme[] Themes = new Theme[]
         {
-            new Theme("Light", new Styles() { AvaloniaFluentBase, AvaloniaFluentBaseLight, AvaloniaDefault, AvaloniaDefaultBaseLight, Styles }),
-            new Theme("Light Blue", new Styles() { AvaloniaFluentBase, AvaloniaFluentBaseLight, AvaloniaDefault, BaseLightBlue, Styles }),
-            new Theme("Dark", new Styles() { AvaloniaFluentBase, AvaloniaFluentBaseDark, AvaloniaDefault, AvaloniaDefaultBaseDark, Styles }),
-            new Theme("Fluent Light", new Styles() { AvaloniaFluentLight, Styles }),
-            new Theme("Fluent Dark", new Styles() { AvaloniaFluentDark, Styles }),
-            new Theme("Citrus", new Styles() { Citrus, Styles }),
-            new Theme("Sea (Citrus)", new Styles() { CitrusSea, Styles }),
-            new Theme("Rust (Citrus)", new Styles() { CitrusRust, Styles }),
-            new Theme("Candy (Citrus)", new Styles() { CitrusCandy, Styles }),
-            new Theme("Magma (Citrus)", new Styles() { CitrusMagma, Styles })
+            new Theme("Light", new Styles() { AvaloniaFluentBase, AvaloniaFluentBaseLight, AvaloniaDefault, AvaloniaDefaultBaseLight, DefaultThemeAccents, Styles }),
+            new Theme("Light Blue", new Styles() { AvaloniaFluentBase, AvaloniaFluentBaseLight, AvaloniaDefault, BaseLightBlue, DefaultThemeAccents, Styles }),
+            new Theme("Dark", new Styles() { AvaloniaFluentBase, AvaloniaFluentBaseDark, AvaloniaDefault, AvaloniaDefaultBaseDark, DefaultThemeAccents, Styles }),
+            new Theme("Fluent Light", new Styles() { AvaloniaFluentLight, FluentThemeAccents, Styles }),
+            new Theme("Fluent Dark", new Styles() { AvaloniaFluentDark, FluentThemeAccents, Styles }),
+            new Theme("Citrus", new Styles() { Citrus, CitrusThemeAccents, Styles }),
+            new Theme("Sea (Citrus)", new Styles() { CitrusSea, CitrusThemeAccents, Styles }),
+            new Theme("Rust (Citrus)", new Styles() { CitrusRust, CitrusThemeAccents, Styles }),
+            new Theme("Candy (Citrus)", new Styles() { CitrusCandy, CitrusThemeAccents, Styles }),
+            new Theme("Magma (Citrus)", new Styles() { CitrusMagma, CitrusThemeAccents, Styles }),
         };
 
         private readonly Application _app;
@@ -72,14 +84,10 @@ namespace DotNet.Properties.Services
 
         public IReadOnlyCollection<ITheme> AvailableThemes { get; }
 
-        private static StyleInclude AvaloniaDefaultAccent(string name) => Style($"avares://Avalonia.Themes.Default/Accents/{name}.xaml");
-
-        private static StyleInclude AvaloniaFluentAccent(string name) => Style($"avares://Avalonia.Themes.Fluent/Accents/{name}.xaml");
-
-        private static StyleInclude CitrusTheme(string name) => Style($"avares://Citrus.Avalonia/{name}.xaml");
-
         private static StyleInclude Style(string source) => Style(new Uri(source));
 
         private static StyleInclude Style(Uri source) => new StyleInclude(source) { Source = source };
+
+        private static StyleInclude Style(Uri baseUri, string source) => new StyleInclude(baseUri) { Source = new Uri(source, UriKind.Relative) };
     }
 }
